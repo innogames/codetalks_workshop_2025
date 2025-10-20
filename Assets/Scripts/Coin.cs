81b2a8ace9b0a3d6d7352b3939ff805f17b5ac4c
Hendrik Rakemann <hendrik.rakemann@innogames.com>
2025-10-20 16:33:42 +0200

    stop timer when a coin goal has been reached

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private Timer timer;
+
+    private void Awake()
+    {
+        this.timer ??= FindFirstObjectByType<Timer>();
+    }
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the coin hit an enemy
         if (other.gameObject.GetComponent<PlayerController>() != null)
         {
+            this.timer.IncreaseCoinCount();
             // Destroy the enemy
             Destroy(gameObject);
             
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,15 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
 using Mono.Cecil;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] private int coinGoal;
+    
     private float passedTime;
     
     public float PassedTime => this.passedTime;
+
+    private int coinsCollected;
+
+    private bool timerIsStopped = false;
     
     private void FixedUpdate()
     {
+        if (this.timerIsStopped)
+            return;
+        
         this.passedTime += Time.fixedDeltaTime;
+    }
+
+    public void IncreaseCoinCount()
+    {
+        this.coinsCollected++;
+        if (this.coinsCollected >= this.coinGoal)
+        {
+            this.timerIsStopped = true;
+        }
     }
 }
